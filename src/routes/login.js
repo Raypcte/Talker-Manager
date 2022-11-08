1b68b2024ed37c89c671f7741f43f92df89d7c26
Raypcte <rayanevalak@gmail.com>
2022-11-08 02:12:15 -0300

    correcao lint do 3 e vai passando tambem o re4

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const login = express.Router();
 
 const crypto = require('crypto');
@@ -6,9 +7,26 @@ const crypto = require('crypto');
 function generateToken() {
   return crypto.randomBytes(8).toString('hex');
 }
+const emailValido = (email) => {
+  const regex = /\w+@\w+\.\w+/;
+  return regex.test(email);
+};
 
 login.post('/login', (request, response) => {
   const { email, password } = request.body;
+
+  if (!email) {
+    return response.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (emailValido(email) === false) {
+    return response.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return response.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length <= 5) {
+    return response.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
   
   return response.status(200).send(`{ "token": "${generateToken()}" }`);
 });
